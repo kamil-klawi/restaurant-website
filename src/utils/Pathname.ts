498a2afa145ac203ca801b2fda1fa5ba67f857ad
Kamil Klawikowski <kamil.klawikowski03@gmail.com>
2023-10-27 13:32:51 +0200

    styles(Pathname): changes the business logic

--- src/utils/Pathname.ts
+++ src/utils/Pathname.ts
@@ -1,3 +1,13 @@
 export function Pathname() {
-  return window.location.pathname.replace('/', '');
+  const path = window.location.pathname.replace('/', '');
+  switch (path) {
+    case 'menu':
+      return 'menu';
+    case 'special_offers':
+      return 'oferty specjalne';
+    case 'store':
+      return 'sklep';
+    case 'contact':
+      return 'kontakt';
+  }
 }
